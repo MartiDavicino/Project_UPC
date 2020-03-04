8d225e7e7a8e858b4b2b652d02251bc0f1268d56
AitorAlvarez17 <43441256+AitorAlvarez17@users.noreply.github.com>
2020-03-04 16:37:17 +0100

    SDLclean

---
+++ SDLclean/Fichero con SDL limpio/Main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "SDL/include/SDL.h"
+#include "SDL/include/SDL_image.h"
+#include "SDL/include/SDL_mixer.h"
+
+
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+
+#pragma comment(lib,"SDL/libx86/SDL2.lib")
+#pragma comment(lib,"SDL/libx86/SDL2main.lib")
+
+int main(int argc, char* argv[]) {
+
+
+
+	system("pause");
+	return 0;
+	
+}
