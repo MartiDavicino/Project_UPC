6ca2d651e3172274e016d31eaadd62168b0e3166
AitorAlvarez17 <43441256+AitorAlvarez17@users.noreply.github.com>
2020-04-08 13:46:54 +0200

    changes

--- Project version 0.001/Main.cpp
+++ Project version 0.001/Main.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL.h>
+#include <iostream>
 #include "SDL/include/SDL.h"
 #include "SDL/include/SDL_image.h"
 #include "SDL/include/SDL_mixer.h"
@@ -14,6 +16,8 @@ const int SCREEN_HEIGHT = 480;
 #pragma comment(lib,"SDL/libx86/SDL2main.lib")
 
 int main(int argc, char* argv[]) {
+
+	Game game;
 
 
 
