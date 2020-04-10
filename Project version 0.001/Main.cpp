82fe7cfba6a1b5d15549622fec54714ea3241e76
AitorAlvarez17 <43441256+AitorAlvarez17@users.noreply.github.com>
2020-04-10 18:39:57 +0200

    some minor changes
    later i will upload the whole thing

--- Project version 0.001/Main.cpp
+++ Project version 0.001/Main.cpp
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <SDL.h>
 #include <iostream>
 #include "SDL/include/SDL.h"
 #include "SDL/include/SDL_image.h"
 #include "SDL/include/SDL_mixer.h"
 
-
-
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-
 #pragma comment(lib,"SDL/libx86/SDL2.lib")
 #pragma comment(lib,"SDL/libx86/SDL2main.lib")
+
+enum class main_states
+{
+	MAIN_CREATION,
+	MAIN_START,
+	MAIN_UPDATE,
+	MAIN_FINISH,
+	MAIN_EXIT
+};
+
 
 int main(int argc, char* argv[]) {
 
