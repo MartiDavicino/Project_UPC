c433cfd3d474cce74e5cecf7f809d46bef30c822
MartiDavicino <60965161+MartiDavicino@users.noreply.github.com>
2020-04-14 17:11:06 +0200

    Balls classes and BallModule added

---
+++ Project versions/PANG version 0.09.5/Project_7_Handout/Source/BigBall.cpp
@@ -0,0 +1,36 @@
+#include "BigBall.h"
+
+#include "Ball.h"
+#include "Path.h"
+
+#include "Application.h"
+#include "ModuleCollisions.h"
+
+BigBall::BigBall(int x, int y) : Ball(x, y)
+{
+	/*flyAnim.PushBack({ 5,6,24,24 });
+	flyAnim.PushBack({ 38, 6, 24, 24 });
+	flyAnim.PushBack({ 71, 6, 24, 24 });
+	flyAnim.PushBack({ 104, 6, 24, 24 });
+	flyAnim.PushBack({ 137, 6, 24, 24 });
+	flyAnim.PushBack({ 170, 6, 24, 24 });
+	flyAnim.PushBack({ 203, 6, 24, 24 });
+	flyAnim.PushBack({ 236, 6, 24, 24 });
+	flyAnim.speed = 0.2f;*/
+
+	/*currentAnim = &flyAnim;*/
+
+	collider = App->collisions->AddCollider({ 0, 0, 24, 24 }, Collider::Type::ENEMY, (Module*)App->enemies);
+}
+
+void BigBall::Update()
+{
+	/*waveRatio += waveRatioSpeed;
+
+	position.y = spawnPos.y + (waveHeight * sinf(waveRatio));
+	position.x -= 1;*/
+
+	// Call to the base class. It must be called at the end
+	// It will update the collider depending on the position
+	Ball::Update();
+}
---
+++ Project versions/PANG version 0.09.5/Project_7_Handout/Source/ModuleParticles.cpp
@@ -0,0 +1,142 @@
+#include "ModuleParticles.h"
+
+#include "Application.h"
+
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModuleCollisions.h"
+
+#include "SDL/include/SDL_timer.h"
+
+ModuleParticles::ModuleParticles()
+{
+	for(uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
+		particles[i] = nullptr;
+}
+
+ModuleParticles::~ModuleParticles()
+{
+
+}
+
+bool ModuleParticles::Start()
+{
+	LOG("Loading particles");
+	texture = App->textures->Load("Assets/rope.png");
+
+	// Explosion particle
+	explosion.anim.PushBack({274, 296, 33, 30});
+	explosion.anim.PushBack({313, 296, 33, 30});
+	explosion.anim.PushBack({346, 296, 33, 30});
+	explosion.anim.PushBack({382, 296, 33, 30});
+	explosion.anim.PushBack({419, 296, 33, 30});
+	explosion.anim.PushBack({457, 296, 33, 30});
+	explosion.anim.loop = false;
+	explosion.anim.speed = 0.3f;
+
+	//X: the first is 20 px width, then each sprite is 17 px width
+	//Y: the first is 33 px height, then each sprite increases the size by 2px
+	rope.anim.PushBack({ 0,0,20,400 });
+	rope.anim.PushBack({ 20,0,17,400 });
+	rope.anim.PushBack({ 37,0,17,400 });
+	rope.anim.PushBack({ 54,0,17,400 });
+	rope.anim.PushBack({ 71,0,17,400 });
+	rope.anim.PushBack({ 88,0,17,400 });
+	rope.anim.PushBack({ 105,0,17,400 });
+	rope.anim.PushBack({ 122,0,17,400 });
+	
+	rope.speed.y = 1;
+	rope.lifetime = 1000;
+	rope.anim.speed = 1.0;
+
+	return true;
+}
+
+bool ModuleParticles::CleanUp()
+{
+	LOG("Unloading particles");
+
+	// Delete all remaining active particles on application exit 
+	for(uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
+	{
+		if(particles[i] != nullptr)
+		{
+			delete particles[i];
+			particles[i] = nullptr;
+		}
+	}
+
+	return true;
+}
+
+void ModuleParticles::OnCollision(Collider* c1, Collider* c2)
+{
+	for (uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
+	{
+		// Always destroy particles that collide
+		if (particles[i] != nullptr && particles[i]->collider == c1)
+		{
+			delete particles[i];
+			particles[i] = nullptr;
+			break;
+		}
+	}
+}
+
+update_status ModuleParticles::Update()
+{
+	for(uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
+	{
+		Particle* particle = particles[i];
+
+		if(particle == nullptr)	continue;
+
+		// Call particle Update. If it has reached its lifetime, destroy it
+		if(particle->Update() == false)
+		{
+			delete particle;
+			particles[i] = nullptr;
+		}
+	}
+
+	return update_status::UPDATE_CONTINUE;
+}
+
+update_status ModuleParticles::PostUpdate()
+{
+	//Iterating all particle array and drawing any active particles
+	for (uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
+	{
+		Particle* particle = particles[i];
+
+		if (particle != nullptr && particle->isAlive)
+		{
+			App->render->Blit(texture, particle->position.x, particle->position.y, &(particle->anim.GetCurrentFrame()));
+		}
+	}
+
+	return update_status::UPDATE_CONTINUE;
+}
+
+void ModuleParticles::AddParticle(const Particle& particle, int x, int y, Collider::Type colliderType, uint delay)
+{
+	for (uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
+	{
+		//Finding an empty slot for a new particle
+		if (particles[i] == nullptr)
+		{
+			Particle* p = new Particle(particle);
+
+			p->frameCount = -(int)delay;			// We start the frameCount as the negative delay
+			p->position.x = x;						// so when frameCount reaches 0 the particle will be activated
+			p->position.y = y;
+
+			//Adding the particle's collider
+			if (colliderType != Collider::Type::NONE)
+				p->collider = App->collisions->AddCollider(p->anim.GetCurrentFrame(), colliderType, this);
+
+			particles[i] = p;
+			break;
+		}
+	}
+}
