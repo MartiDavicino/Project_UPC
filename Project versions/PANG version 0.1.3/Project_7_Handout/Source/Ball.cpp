52a9e20c52dfdc042522f3b43646958a3fe3bce1
MartiDavicino <60965161+MartiDavicino@users.noreply.github.com>
2020-04-17 20:16:46 +0200

    God mode and part of dividing balls function

---
+++ Project versions/PANG version 0.1.3/Project_7_Handout/Source/Ball.cpp
@@ -0,0 +1,57 @@
+#include "Ball.h"
+
+#include "Application.h"
+#include "ModuleCollisions.h"
+#include "ModuleParticles.h"
+#include "ModuleAudio.h"
+#include "ModuleRender.h"
+
+Ball::Ball(int x, int y) : position(x, y)
+{
+	spawnPos = position;
+}
+
+Ball::~Ball()
+{
+	if (collider != nullptr)
+		collider->pendingToDelete = true;
+}
+
+const Collider* Ball::GetCollider() const
+{
+	return collider;
+}
+
+void Ball::Update()
+{
+	if (currentAnim != nullptr)
+		currentAnim->Update();
+
+	if (collider != nullptr)
+		collider->SetPos(position.x, position.y);
+}
+
+void Ball::Draw()
+{
+	if (currentAnim != nullptr)
+		App->render->Blit(texture, position.x, position.y, &(currentAnim->GetCurrentFrame()));
+}
+
+void Ball::OnCollision(Collider* collider)
+{
+	LOG("Ball touched wall\n");
+
+	App->particles->AddParticle(App->particles->bigExplosion, position.x, position.y);
+	App->audio->PlayFx(destroyedFx);
+
+	Ball_vx = -Ball_vx;
+
+	SetToDelete();
+}
+
+void Ball::SetToDelete()
+{
+	pendingToDelete = true;
+	if (collider != nullptr)
+		collider->pendingToDelete = true;
+}
