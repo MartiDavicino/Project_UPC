92f66f01c3e0b1a2bf0ea839fc24c108a665a44f
MartiDavicino <60965161+MartiDavicino@users.noreply.github.com>
2020-04-19 17:58:19 +0200

    1.5.2

---
+++ Project versions/PANG version 0.1.5.2/Project_7_Handout/Source/ModuleBalls.h
@@ -0,0 +1,93 @@
+#ifndef __MODULEBALLSH__
+#define __MODULEBALLS_H__
+
+#include "Module.h"
+#include "Ball.h"
+#include "ModuleParticles.h"
+
+#define MAX_BALLS 100
+
+// TODO 4: Create a new enemy type: the Mech
+
+
+
+struct BallSpawnpoint
+{
+	BALL_TYPE type = BALL_TYPE::NO_TYPE;
+	int x, y;
+};
+
+class Ball;
+struct SDL_Texture;
+
+class ModuleBalls : public Module
+{
+public:
+	// Constructor
+	ModuleBalls(bool startEnabled);
+
+	// Destructor
+	~ModuleBalls();
+
+	// Called when the module is activated
+	// Loads the necessary textures for the enemies
+	bool Start() override;
+
+	update_status PreUpdate() override;
+	// Called at the middle of the application loop
+	// Handles all enemies logic and spawning/despawning
+	update_status Update() override;
+
+	// Called at the end of the application loop
+	// Iterates all the enemies and draws them
+	update_status PostUpdate() override;
+
+	// Called on application exit
+	// Destroys all active enemies left in the array
+	bool CleanUp() override;
+
+	// Called when an enemi collider hits another collider
+	// The enemy is destroyed and an explosion particle is fired
+	void OnCollision(Collider* c1, Collider* c2) override;
+
+	// Add an enemy into the queue to be spawned later
+	bool AddBall(BALL_TYPE type, int x, int y);
+
+	// Iterates the queue and checks for camera position
+	void HandleBallsSpawn();
+
+	// Destroys any enemies that have moved outside the camera limits
+	void HandleBallsDespawn();
+
+	void DivideBalls(Ball ball);
+
+
+	int Xoffset = 10;
+	int Yoffset = 0;
+
+	//collisions
+	bool Collision_A;
+	bool Collision_D;
+	bool Collision_F;
+
+private:
+	// Spawns a new enemy using the data from the queue
+	void SpawnBall(const BallSpawnpoint& info);
+
+	
+
+private:
+	// A queue with all spawn points information
+	BallSpawnpoint spawnQueue[MAX_BALLS];
+
+	// All spawned enemies in the scene
+	Ball* Balls[MAX_BALLS] = { nullptr };
+
+	// The enemies sprite sheet
+	SDL_Texture* texture = nullptr;
+
+	// The audio fx for destroying an enemy
+	int enemyDestroyedFx = 0;
+};
+
+#endif // __MODULE_ENEMIES_H__
---
+++ Project versions/PANG version 0.1.5.2/Project_7_Handout/Source/ModuleScene.cpp
@@ -0,0 +1,88 @@
+#include "ModuleScene.h"
+
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModuleAudio.h"
+#include "ModuleCollisions.h"
+#include "ModuleEnemies.h"
+#include "ModuleBalls.h"
+#include "ModulePlayer.h"
+
+ModuleScene::ModuleScene(bool startEnabled) : Module(startEnabled)
+{
+
+}
+
+ModuleScene::~ModuleScene()
+{
+
+}
+
+// Load assets
+bool ModuleScene::Start()
+{
+	LOG("Loading background assets");
+
+	bool ret = true;
+
+	bgTexture = App->textures->Load("Assets/01.png");
+	/*SDL_Rect section = &bgTexture;*/
+	//App->audio->PlayMusic("Assets/stage1.ogg", 1.0f);
+
+	//Bottomside collider
+	App->collisions->AddCollider({ 0, 224, 3930, 16 }, Collider::Type::FLOOR);
+
+	//First two columns colliders
+	App->collisions->AddCollider({ 0, 0,7,300 }, Collider::Type::WALL_A);
+	App->collisions->AddCollider({ 377, 0, 7, 300 }, Collider::Type::WALL_D);
+	App->collisions->AddCollider({ 0,201,384,8 }, Collider::Type::FLOOR);
+
+	
+	//Balls
+	App->balls->AddBall(BALL_TYPE::BIG, 150, 25);
+	/*App->balls->AddBall(BALL_TYPE::MEDIUM, 100, 25);*/
+	//App->balls->AddBall(BALL_TYPE::SMALL, 50, 25);
+	//App->balls->AddBall(BALL_TYPE::TINY, 35, 25);
+
+	App->player->Enable();
+	App->balls->Enable();
+
+
+
+	return ret;
+}
+
+update_status ModuleScene::Update()
+{
+	//App->render->camera.x += 3;
+
+	return update_status::UPDATE_CONTINUE;
+}
+
+// Update: draw background
+update_status ModuleScene::PostUpdate()
+{
+	
+
+	// Draw everything --------------------------------------
+	App->render->Blit(bgTexture,0,0,NULL);
+
+	return update_status::UPDATE_CONTINUE;
+}
+
+bool ModuleScene::CleanUp()
+{
+	// TODO 2: Enable (and properly disable) the player module
+	App->player->Disable();
+	App->balls->Disable();
+	App->textures->CleanUp();
+	App->balls->CleanUp();
+	App->collisions->CleanUp();
+	App->player->CleanUp();
+
+
+	// TODO 5: Remove All Memory Leaks - no solution here guys ;)
+
+	return true;
+}
